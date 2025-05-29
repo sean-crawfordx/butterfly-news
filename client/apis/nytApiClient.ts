86f328b0743462b3b24cbdff1809552f61169ffc
Eric Oh <dhgnlrms99@gmail.com>
2025-05-29 21:00:47 +1200

    feat: news-apis

--- client/apis/nytApiClient.ts
+++ client/apis/nytApiClient.ts
@@ -1,2 +1,12 @@
 // Front end api call functions for nytimes
 // Make a request to the server's proxy route, NOT the external api directly.
+
+import request from 'superagent'
+import { News } from '../../models/news'
+
+const rootURL = new URL(`/api/v1`, document.baseURI)
+
+export async function getMovies() {
+  const response = await request.get(`${rootURL}/movies`)
+  return response.body as News
+}
--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,17 +1,17 @@
 // All other components will be exported into this component in some way - make use of react routing and <Outlet /> s.
 
 import FrontEndTesting from './FrontEndTesting'
+import News from './StoryPreviewBox'
 
 function App() {
   return (
     <>
       <div className="app">
         <FrontEndTesting />
+        <News />
       </div>
     </>
   )
 }
 
 export default App
-
-
